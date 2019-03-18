7ca8782a749a5e53814eb28d484f72583e49f1bb
Gabe <geblarot96@gmail.com>
2019-03-18 17:14:29 -0500

    El modelo

---
+++ Voladores/src/model/Superman.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Superman implements Flyer, Animal, Kryptonian
+{
+
+	@Override
+	public void takeOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void land() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void leapBuilding() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopBullet() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
